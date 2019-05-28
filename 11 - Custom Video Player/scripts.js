cf97a8d4f2367c2fed99ff6ee8a0a77de302dc55
Karim Daghari <karimdxy@gmail.com>
2019-05-28 15:19:45 +0100

    added space bar as controller

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,15 +6,29 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
+let isPaused = true;
+
 function togglePlay() {
     this.paused ? video.play() : video.pause();
+    isPaused = this.paused;
+}
+
+function togglePlaySpace() {
+    isPaused ? video.play() : video.pause();
+    isPaused = !isPaused;
 }
 
 function updateButton() {
-    toggle.textContent = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = isPaused ? '►' : '❚ ❚';
 }
 
+// Play/Pause when the 'video' is clicked
 video.addEventListener('click', togglePlay);
 
+// Play/Pause when the 'space bar' is pressed
+window.addEventListener('keydown', (e) => e.code === "Space" && togglePlaySpace());
+
+// Update the play/pause button display
 video.addEventListener('click', updateButton);
 toggle.addEventListener('click', updateButton);
+window.addEventListener('keydown', (e) => e.code === "Space" && updateButton());
